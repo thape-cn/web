118a929accd52a27a3f0f46ca9403b69ca21ace6
Eric-Guo <eric.guocz@gmail.com>
2020-03-24 15:21:31 +0800

    Add background to home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
   def show
-    flash[:alert] = "Hello TailwindUI!"
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,9 +12,10 @@ require("channels")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const importAll = (r) => r.keys().map(r)
+const images = require.context('../images', false, /\.(png|jpe?g|svg)$/i)
+importAll(images)
+const imagePath = (name) => images(name, true)
 
 import "controllers"
 import "stylesheets/application"
