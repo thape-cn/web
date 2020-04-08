ebcfa19fcf2ec5bfbe1b527c96bc2034d9a9dc59
Eric-Guo <eric.guocz@gmail.com>
2020-04-08 14:22:54 +0800

    Give warning for IE.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def show
+    if browser.ie?
+      flash.now[:alert] = "本站点推荐在Chrome等非IE浏览器下浏览，Chrome浏览器可以在<a href='https://www.google.cn/chrome/'>https://www.google.cn/chrome/</a>下载。".html_safe
+    end
   end
 end
