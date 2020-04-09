fb03c799b97a8fd58a276b3e4616553b614d5580
Eric-Guo <eric.guocz@gmail.com>
2020-04-09 09:36:21 +0800

    Using montserrat as sans

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,6 +1,11 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
       screens: {
         'xxl': '1536px',
       },
