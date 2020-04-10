51e3ee5a756a7dc8276a3780847c39fc5614d513
Eric-Guo <eric.guocz@gmail.com>
2020-04-10 15:45:26 +0800

    Add more

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
     extend: {
+      colors: {
+        'black6c': '#101820',
+        'gray1c': '#d9d9d6',
+        'thape-white-t80': 'rgb(255,255,255,0.8)',
+        'thape-guest-gray': 'rgb(184,184,184,1)',
+        'thape-copyright-gray': 'rgb(140,140,140,1)',
+        'thape-icon-gray': 'rgb(149,149,149,1)'
+      },
       fontFamily: {
         sans: ['Montserrat', 'Source Han Sans SC', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Helvetica Neue', 'PingFang SC', 'Microsoft YaHei', 'Noto Sans CJK SC', 'WenQuanYi Micro Hei', 'sans-serif'],
       },
