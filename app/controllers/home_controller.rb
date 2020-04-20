bb98c8ba4e08c91aeb8b86368b0871b380d72db2
Eric-Guo <eric.guocz@gmail.com>
2020-04-20 17:26:22 +0800

    Separate the home page layout.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  layout 'home_page'
   def show
     if browser.ie?
       flash.now[:alert] = "本站点推荐在Chrome等非IE浏览器下浏览，Chrome浏览器可以在<a href='https://www.google.cn/chrome/'>https://www.google.cn/chrome/</a>下载。".html_safe
