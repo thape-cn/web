cd6d8029768cba5e77c9758379d1fdce099689ca
Eric-Guo <eric.guocz@gmail.com>
2020-04-26 14:25:35 +0800

    Add breadcrumb

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
     extend: {
       colors: {
         'black6c': '#101820',
-        'black6c-t70': 'rgb(16,24,32,0.7)',
+        'black6c-t70': 'rgba(16,24,32,0.7)',
         'gray1c': '#d9d9d6',
-        'thape-white-t80': 'rgb(255,255,255,0.8)',
-        'thape-white-t95': 'rgb(255,255,255,0.95)',
-        'thape-guest-gray': 'rgb(184,184,184,1)',
-        'thape-copyright-gray': 'rgb(140,140,140,1)',
-        'thape-icon-gray': 'rgb(149,149,149,1)'
+        'thape-white-t80': 'rgba(255,255,255,0.8)',
+        'thape-white-t95': 'rgba(255,255,255,0.95)',
+        'thape-guest-gray': 'rgba(184,184,184,1)',
+        'thape-copyright-gray': 'rgba(140,140,140,1)',
+        'thape-icon-gray': 'rgba(149,149,149,1)'
       },
       fontFamily: {
         sans: ['Montserrat', 'Source Han Sans SC', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Helvetica Neue', 'PingFang SC', 'Microsoft YaHei', 'Noto Sans CJK SC', 'WenQuanYi Micro Hei', 'sans-serif'],
