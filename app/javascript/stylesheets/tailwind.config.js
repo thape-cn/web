e2a450f830b7fc9399b666616c85479fecad3f42
Eric-Guo <eric.guocz@gmail.com>
2020-05-09 12:19:56 +0800

    New text style in works.

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -5,14 +5,15 @@ module.exports = {
         'black6c': '#101820',
         'black6c-t70': 'rgba(16,24,32,0.7)',
         'gray1c': '#d9d9d6',
-        'form-border-color': '#ebebeb',
+        'thape-dark-gray': '#ebebeb',
         'form-background': '#fcfdff',
         'thape-white-t80': 'rgba(255,255,255,0.8)',
         'thape-white-t95': 'rgba(255,255,255,0.95)',
         'thape-guest-gray': 'rgba(184,184,184,1)',
         'thape-copyright-gray': 'rgba(140,140,140,1)',
         'thape-footer-gray': 'rgba(235,235,235,1)',
-        'thape-icon-gray': 'rgba(149,149,149,1)'
+        'thape-icon-gray': 'rgba(149,149,149,1)',
+        'thape-darker-gray': 'rgba(158,158,158,1)',
       },
       fontFamily: {
         sans: ['Montserrat', 'Source Han Sans SC', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Helvetica Neue', 'PingFang SC', 'Microsoft YaHei', 'Noto Sans CJK SC', 'WenQuanYi Micro Hei', 'sans-serif'],
