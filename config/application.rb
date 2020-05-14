e999a96a310c7237f5cdb67e49a71f28ea30cd5f
Eric-Guo <eric.guocz@gmail.com>
2020-05-14 14:01:32 +0800

    Add SWITCHBOARD

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module Web
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.default_locale = 'cn'
+    config.i18n.available_locales = ['en', 'cn']
   end
 end
