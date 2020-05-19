9fe358f114b5c24e0abcde06e24e48c86e4d9f77
Eric-Guo <eric.guocz@gmail.com>
2020-05-19 11:22:38 +0800

    New map contact added.

--- app/controllers/biz_maps_controller.rb
+++ app/controllers/biz_maps_controller.rb
@@ -3,5 +3,11 @@ class BizMapsController < ApplicationController
     @m_易爱迪 = MapContact.find(23)
     @m_爱坤 = MapContact.find(24)
     @m_上海室内 = MapContact.find(25)
+    @m_上海城规 = MapContact.find(27)
+    @m_上海园林 = MapContact.find(28)
+    @m_虹核审图 = MapContact.find(29)
+    @m_易术家互娱 = MapContact.find(26)
+    @m_易衡光伏 = MapContact.find(30)
+    @m_易湃环境 = MapContact.find(31)
   end
 end
