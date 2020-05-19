8b52b25dd9e7f49213851fda9868ba0e59add10d
Eric-Guo <eric.guocz@gmail.com>
2020-05-19 10:32:21 +0800

    Add 爱坤

--- app/controllers/biz_maps_controller.rb
+++ app/controllers/biz_maps_controller.rb
@@ -1,5 +1,6 @@
 class BizMapsController < ApplicationController
   def show
     @m_易爱迪 = MapContact.find(23)
+    @m_爱坤 = MapContact.find(24)
   end
 end
--- app/models/map_contact.rb
+++ app/models/map_contact.rb
@@ -1,3 +1,3 @@
 class MapContact < ApplicationRecord
-  translates :name, :long_name, :address, :tel, :fax
+  translates :name, :long_name, :alt_name, :address, :tel, :fax, :website_name
 end
