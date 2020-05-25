8eec6d6a67d77ecdd24abe2fb8446424eb09a72f
Eric-Guo <eric.guocz@gmail.com>
2020-05-25 17:20:03 +0800

    Finish the news controller.

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,5 +4,6 @@ class NewsController < ApplicationController
   end
 
   def index
+    @infos = Info.where(hide_in_index_news: false).order(position: :asc).limit(20)
   end
 end
--- app/models/info.rb
+++ app/models/info.rb
@@ -11,10 +11,6 @@ class Info < ApplicationRecord
 
   before_create :set_new_position
 
-  def self.category_options
-    [['公司新闻', 1], ['行业会议', 2], ['专业奖项', 3]]
-  end
-
   private
 
   def set_new_position
