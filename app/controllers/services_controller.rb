ffbdd4c0f1ac0ba457d8cdd4f247ae8481ae522e
Eric-Guo <eric.guocz@gmail.com>
2020-06-01 17:59:49 +0800

    Layout one.

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -3,7 +3,7 @@ class ServicesController < ApplicationController
 
   def building
     render 'services', locals: {
-      background_img: 'media/images/building.jpg',
+      background_img: 'media/images/building.jpg', service_mt: 'mt-6',
       chinese_title: '建筑设计',
       english_title: 'ARCHITECTURE',
       p1: '天华建筑，秉持“创意与技术完美融合”的品牌理念，致力于汇聚并培养专业人才团队，打造“体验与整合”为导向的创新设计。通过21个主要城市的分支机构网络，服务全国的客户和项目，专注营造品质生活空间、城市空间、文化空间、工作空间等，提供建筑、结构及机电工程咨询与设计全过程服务，涉足领域包括：住宅、商业、办公、城市综合体、城市更新、文化、医养、教育、旅居酒店等。',
@@ -13,7 +13,7 @@ class ServicesController < ApplicationController
 
   def interior
     render 'services', locals: {
-      background_img: 'media/images/interior.jpg',
+      background_img: 'media/images/interior.jpg', service_mt: 'mt-3',
       chinese_title: '室内设计',
       english_title: 'INTERIOR',
       p1: '“天华·易筑”是天华集团创意业务板块室内设计与工程业务的重要组成部分，凭借其独特的创意设计能力、技术落地能力、项目管理能力、成本控制能力以及服务响应能力赢得了业界的一致认可和信任。为了满足不断增加的业务需求，“天华·易筑”在2013年初独立成为上海天华室内设计有限公司，并于2018年成立深圳和武汉区域子公司负责拓展华南和华中市场。 “天华·易筑”致力于办公空间、居住空间、商业零售空间、会所和售楼处等的高端室内空间的营造。',
@@ -25,7 +25,7 @@ class ServicesController < ApplicationController
 
   def planning
     render 'services', locals: {
-      background_img: 'media/images/planning.jpg',
+      background_img: 'media/images/planning.jpg', service_mt: 'mt-4',
       chinese_title: '城市规划',
       english_title: 'URBAN PLANNING',
       p1: '天华规划，秉持以责任规划城市的可持续发展理念，以国际化大都市先进发展经验为基础，鼓励跨行业多视角的合作，追求资源整合效率的最大化，并致力于成为全球最佳城市发展咨询合作伙伴。天华规划年轻而有活力的团队毕业于世界各地的一流院校，拥有多名博士及博士后，硕士学历超过三分之二，视野兼具专业深度与全球广度。',
@@ -35,7 +35,7 @@ class ServicesController < ApplicationController
 
   def landscape
     render 'services', locals: {
-      background_img: 'media/images/landscape.jpg',
+      background_img: 'media/images/landscape.jpg', service_mt: 'mt-12',
       chinese_title: '景观设计',
       english_title: 'LANDSCAPE',
       p1: '天华景观凭借优秀的设计水准及专业完善的服务赢得业界好评，积累了众多知名客户长期合作伙伴，项目涵盖居住区景观、公建景观、绿地公园等各领域。全新打造的天华景观团队，具有国际化的队伍组合，完善的技术力量，全程化的总体服务，严谨的工作流程。',
@@ -45,7 +45,7 @@ class ServicesController < ApplicationController
 
   def vetting
     render 'services', locals: {
-      background_img: 'media/images/vetting.jpg',
+      background_img: 'media/images/vetting.jpg', service_mt: 'mt-12',
       chinese_title: '审图',
       english_title: 'DRAWING REVIEW',
       p1: '虹核审图业务团队主营建设工程审图，2001年获得上海市建交委员会颁发的上海市建设工程施工图设计文件审查（一类含超限高层）机构认定资质，是上海市最早的11家审图机构之一。并于2006年12月获得ISO9001质量体系认证证书，建有一套质量保证体系和管理标准。审查总量在全市审查机构中名列前茅，并多年被主管部门评为先进单位。',
@@ -55,7 +55,7 @@ class ServicesController < ApplicationController
 
   def consluting
     render 'services', locals: {
-      background_img: 'media/images/consluting.jpg',
+      background_img: 'media/images/consluting.jpg', service_mt: 'mt-10',
       chinese_title: '技术咨询',
       english_title: 'CONSULTATION',
       p1: '基于天华在民用建筑设计领域数十年的耕耘，天华技术咨询厚积薄发，源于设计，高于设计，可提供项目全生命周期的技术顾问咨询服务，服务范围涵盖选型优化、效果实现、成本预控、超高超限分析、新技术研发和应用（PC、BIM、钢结构...）、 标准编制、第三方审核、抗震审批、更新改建等。',
@@ -65,7 +65,7 @@ class ServicesController < ApplicationController
 
   def vrtech
     render 'services', locals: {
-      background_img: 'media/images/vrtech.jpg',
+      background_img: 'media/images/vrtech.jpg', service_mt: 'mt-7',
       chinese_title: '可视化',
       english_title: 'VR TECHNOLOGY',
       p1: 'VR（虚拟现实）是当下最热门的前沿技术之一，VR在建筑领域的应用将给地产行业带来全新的思维模式和工作流程。易术家是建筑VR内容领域的先行者，定位于包括 VR / AR 在内的各种新技术在房地产市场和设计表现领域的垂直细分市场，致力于结合新兴的科技手段和集团在业内领先的建筑美学表达经验，提供高品质的VR产品和服务，着力解决行业需求和改革现有的表达方式。',
