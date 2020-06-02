35b71df812d74d699532d38b4545bb7de276db5f
Eric-Guo <eric.guocz@gmail.com>
2020-06-02 09:57:20 +0800

    Remove more company in biz-map.

--- app/controllers/biz_maps_controller.rb
+++ app/controllers/biz_maps_controller.rb
@@ -1,14 +1,11 @@
 class BizMapsController < ApplicationController
   def show
-    @m_易爱迪 = MapContact.find(23)
     @m_爱坤 = MapContact.find(24)
     @m_上海室内 = MapContact.find(25)
     @m_上海城规 = MapContact.find(27)
     @m_上海园林 = MapContact.find(28)
     @m_虹核审图 = MapContact.find(29)
     @m_易术家互娱 = MapContact.find(26)
-    @m_易衡光伏 = MapContact.find(30)
-    @m_易湃环境 = MapContact.find(31)
     @m_北京天华 = MapContact.find(3)
     @m_深圳天华 = MapContact.find(9)
     @m_广州天华 = MapContact.find(17)
