5d5351aec1e198d4348734fe41e09ab012f6a4e3
Eric-Guo <eric.guocz@gmail.com>
2020-06-05 13:14:13 +0800

    Remove no need JS.

--- app/javascript/controllers/works_controller.js
+++ app/javascript/controllers/works_controller.js
@@ -2,7 +2,5 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   connect() {
-    const navWorksItem = document.getElementById('nav-works-item');
-    navWorksItem.classList.add("text-black6c");
   }
 }
