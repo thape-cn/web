e9da2f119cc8fe22128a5d679df9f331f2e7acac
Eric-Guo <eric.guocz@gmail.com>
2020-06-09 09:31:05 +0800

    Add search facility to the area leadership.

--- app/controllers/leadership_controller.rb
+++ app/controllers/leadership_controller.rb
@@ -1,31 +1,44 @@
 class LeadershipController < ApplicationController
   def index
     @search_name = params[:name].presence
-    jituan_people = Person.joins('INNER JOIN person_translations ON person_translations.person_id = people.id').includes(city_people: :city).where(cities: { name: '集团'})
+    jituan_people = Person.includes(city_people: :city).where(cities: { name: '集团'})
       .where(leaving_date: nil).order(position: :asc).limit(12)
     @management_people = if @search_name.present?
-      jituan_people.where(category: 1).where('person_translations.name like ?', "%#{@search_name}%")
+      jituan_people.joins('INNER JOIN person_translations ON person_translations.person_id = people.id')
+        .where(category: 1).where('person_translations.name like ?', "%#{@search_name}%")
     else
       jituan_people.where(category: 1)
     end
 
 
     @speciality_people = if @search_name.present?
-      jituan_people.where(category: 2).where('people.name like ?', "%#{@search_name}%")
+      jituan_people.joins('INNER JOIN person_translations ON person_translations.person_id = people.id')
+        .where(category: 2).where('person_translations.name like ?', "%#{@search_name}%")
     else
       jituan_people.where(category: 2)
     end
   end
 
   def show
+    @search_name = params[:name].presence
     case params[:id]
     when "shanghai"
       shanghai_people = Person.includes(city_people: :city).where(cities: { name: '上海'})
-      @management_people = shanghai_people.where(leaving_date: nil).where(category: 1).where(belong_area: '上海')
+        .where(leaving_date: nil).where(belong_area: '上海')
         .order(position: :asc).limit(12)
+      @management_people = if @search_name.present?
+        shanghai_people.joins('INNER JOIN person_translations ON person_translations.person_id = people.id')
+          .where(category: 1).where('person_translations.name like ?', "%#{@search_name}%")
+      else
+        shanghai_people.where(category: 1)
+      end
 
-      @speciality_people = shanghai_people.where(leaving_date: nil).where(category: 2).where(belong_area: '上海')
-        .order(position: :asc).limit(12)
+      @speciality_people = if @search_name.present?
+        shanghai_people.joins('INNER JOIN person_translations ON person_translations.person_id = people.id')
+          .where(category: 2).where('person_translations.name like ?', "%#{@search_name}%")
+      else
+        shanghai_people.where(category: 2)
+      end
       render 'area_leadership', locals: { c: '上海', e: 'SHANGHAI' }
     else
       @person = Person.where(leaving_date: nil).find_by(id: params[:id]) \
