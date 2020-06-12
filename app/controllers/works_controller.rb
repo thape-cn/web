52bb2ae30d594eba4759fa7873642616a5fbdbb1
Eric-Guo <eric.guocz@gmail.com>
2020-06-12 09:33:30 +0800

    Link to DB for residential-rental

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -9,5 +9,8 @@ class WorksController < ApplicationController
   end
 
   def residential_rental
+    @works = Work.includes(work_project_types: :project_type, work_residential_types: :residential_type)
+      .where(work_project_types: { project_types: { cn_name: '居住' } })
+      .where(work_residential_types: { residential_types: { cn_name: '租赁住房' } })
   end
 end
