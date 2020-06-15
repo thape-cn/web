fc3517a99d451490dea5ae1d5b476949910f17dc
Eric-Guo <eric.guocz@gmail.com>
2020-06-15 17:37:12 +0800

    Link to project type url.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,34 @@
 module ApplicationHelper
+  def project_type_path(project_type)
+    case project_type.cn_name
+    when "居住"
+      residential_works_path
+    when "展示区/示范区"
+      demonstration_zone_works_path
+    when "商务办公"
+      office_works_path
+    when "超高层"
+      supertall_works_path
+    when "商业空间"
+      commercial_works_path
+    when "综合体/TOD"
+      mixed_used_tod_works_path
+    when "文化空间"
+      cultural_works_path
+    when "医疗康养"
+      medical_care_works_path
+    when "旅居/酒店"
+      hospitality_works_path
+    when "教育"
+      education_works_path
+    when "城市设计/更新"
+      urban_design_works_path
+    when "城市规划"
+      urban_planning_works_path
+    when "景观"
+      landscape_works_path
+    when "室内"
+      interior_works_path
+    end
+  end
 end
