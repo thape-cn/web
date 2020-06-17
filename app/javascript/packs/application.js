2846a458c094cc524a581acab9481bbe83220198
Eric-Guo <eric.guocz@gmail.com>
2020-06-17 11:17:03 +0800

    Add stimulus/polyfills to make it works in IE 11

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import "@stimulus/polyfills";
+
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
