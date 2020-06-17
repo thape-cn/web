d723af1924d48c984814939e79c72792452350db
Eric-Guo <eric.guocz@gmail.com>
2020-06-17 17:22:20 +0800

    Return mobile picture for mobile.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,20 +4,29 @@ class HomeController < ApplicationController
     @support_webp = browser.chrome? || browser.firefox?
     @wallpapers = []
 
-    if @support_webp
-      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/01_background.webp')
-      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/02_background.webp')
-      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/03_background.webp')
-      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/04_background.webp')
-      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/05_background.webp')
-      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/06_background.webp')
+    if browser.mobile?
+      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/01_background_mobile.jpg')
+      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/02_background_mobile.jpg')
+      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/03_background_mobile.jpg')
+      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/04_background_mobile.jpg')
+      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/05_background_mobile.jpg')
+      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/06_background_mobile.jpg')
     else
-      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/01_background.jpg')
-      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/02_background.jpg')
-      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/03_background.jpg')
-      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/04_background.jpg')
-      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/05_background.jpg')
-      @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/06_background.jpg')
+      if @support_webp
+        @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/01_background.webp')
+        @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/02_background.webp')
+        @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/03_background.webp')
+        @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/04_background.webp')
+        @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/05_background.webp')
+        @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/06_background.webp')
+      else
+        @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/01_background.jpg')
+        @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/02_background.jpg')
+        @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/03_background.jpg')
+        @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/04_background.jpg')
+        @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/05_background.jpg')
+        @wallpapers << ActionController::Base.helpers.asset_pack_path('media/images/06_background.jpg')
+      end
     end
 
     @home_page_content = HomePageContent.first
