37358a70335b43a129809032f19a6379d056e9c5
Eric-Guo <eric.guocz@gmail.com>
2020-06-18 11:15:32 +0800

    Handle remain problem in works after adding project type select.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,12 +1,14 @@
 class WorksController < ApplicationController
   def index
     @project_type = {}
+    @self_path = works_path
   end
 
   def show
     @city = City.find_by url_name: params[:id]
     if @city.present?
       @project_type = {}
+      @self_path = work_path(id: @city.url_name)
       render :area_detail
     else
       @work = Work.find params[:id]
@@ -98,21 +100,25 @@ class WorksController < ApplicationController
 
   def residential
     @project_type = ProjectType.find_by cn_name: '居住'
+    @self_path = demonstration_zone_works_path
   end
 
   def residential_residence
+    @project_type = ProjectType.find_by cn_name: '居住'
     @residential_type = ResidentialType.find_by cn_name: '精品住宅'
     @self_path = residential_residence_works_path
     render_residential
   end
 
   def residential_community
+    @project_type = ProjectType.find_by cn_name: '居住'
     @residential_type = ResidentialType.find_by cn_name: '综合社区'
     @self_path = residential_community_works_path
     render_residential
   end
 
   def residential_rental
+    @project_type = ProjectType.find_by cn_name: '居住'
     @residential_type = ResidentialType.find_by cn_name: '租赁住房'
     @self_path = residential_rental_works_path
     render_residential
