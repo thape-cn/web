469f4b68386a22b51abcbbfbac119cc8770aee78
秦嘉鹏 <qinjiapeng@3lengjing.com>
2020-06-22 23:59:29 +0800

    优化服务页面

--- app/javascript/controllers/service_controller.js
+++ app/javascript/controllers/service_controller.js
@@ -4,19 +4,24 @@ export default class extends Controller {
   static targets = [ "bgImg", "introDiv" ]
 
   resize() {
-    if (this.bgImgTarget.height >= window.innerHeight) {
-      this.introDivTarget.classList.add("bg-thape-white-t80");
-      this.introDivTarget.classList.remove("bg-white");
+    if (this.bgImgTarget.offsetHeight >= this.element.offsetHeight) {
+      this.introDivTarget.classList.add("bg-thape-white-t80", 'absolute');
+      this.introDivTarget.classList.remove("bg-white", 'flex', 'content-center', 'flex-auto');
     } else {
       this.introDivTarget.classList.remove("bg-thape-white-t80");
       this.introDivTarget.classList.add("bg-white");
+      if (this.bgImgTarget.offsetHeight + this.introDivTarget.offsetHeight <= this.element.offsetHeight) {
+        this.introDivTarget.classList.remove('absolute');
+        this.introDivTarget.classList.add('flex', 'content-center', 'flex-auto');
+      } else {
+        this.introDivTarget.classList.remove('flex', 'content-center', 'flex-auto');
+        this.introDivTarget.classList.add('absolute');
+      }
     }
   }
 
   connect() {
-    setTimeout(() => {
-      this.resize();
-    }, 200)
+    this.resize();
   }
 
   layout() {
