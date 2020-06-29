a5876e155017922670c7e619e74a03c08d5138bf
Eric-Guo <eric.guocz@gmail.com>
2020-06-29 08:30:33 +0800

    Change structure to add back scrollama home controller.

--- app/javascript/controllers/home_ie11_controller.js
+++ app/javascript/controllers/home_ie11_controller.js
@@ -1,12 +1,11 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ 'waypoint' ];
   connect() {
     const Waypoint = window.Waypoint;
     for (let i = 0; i < 10; i++) {
       new Waypoint({
-        element: this.element,
+        element: document.getElementById('thape-home-waypoint'),
         handler: function(direction) {
           this.element.style.opacity = direction === 'down' ? 0.1 * i + 0.1 : 0.1 * i;
         },
