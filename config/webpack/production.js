83209e02521d8c4df29a07aafa8798823da59d07
qinjiapeng <qinjia[eng@3lengjing.com>
2020-07-02 19:01:00 +0800

    更改webpack文件体积限制

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+const config = environment.toWebpackConfig();
+
+module.exports = {
+    ...config,
+    performance: {
+        //入口起点的最大体积
+        maxEntrypointSize: 50000000,
+        //生成文件的最大体积
+        maxAssetSize: 30000000,
+    }
+}
