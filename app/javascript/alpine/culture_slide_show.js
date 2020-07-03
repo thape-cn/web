31a90aca49baa9dd944984135c31d2666046b7c0
Eric-Guo <eric.guocz@gmail.com>
2020-07-03 22:29:37 +0800

    Slide the culture.

--- app/javascript/alpine/culture_slide_show.js
+++ app/javascript/alpine/culture_slide_show.js
@@ -1,27 +1,23 @@
-window.cultureSlideShow = function(slides) {
+window.cultureSlideShow = function() {
     return {
         state: {
             moving: false,
-            currentSlide: 0,
+            currentSlide: 1,
             looping: false,
             order: [],
-            nextSlideDirection: '',
-            userInteracted: false,
-            usedKeyboard: false,
+            nextSlideDirection: ''
         },
         autoplayTimer: null,
         attributes: {
             direction: 'right-left',
             duration: 1000,
-            timer: 7000,
+            timer: 4000,
         },
-        slides: [],
+        slides: [1,2,3,4],
         setup() {
-            this.slides = slides.map((slide, index) => { slide.id = index + Date.now(); return slide })
-
             // Cache the original order so that we can reorder on transition (to skip inbetween slides)
-            this.state.order = this.slides.map(slide => slide.id)
-            const newSlideOrder = this.slides.filter(slide => this.current.id != slide.id)
+            this.state.order = this.slides
+            const newSlideOrder = this.slides.filter(slide_id => this.current != slide_id)
             newSlideOrder.unshift(this.current)
             this.slides = newSlideOrder
 
@@ -29,24 +25,24 @@ window.cultureSlideShow = function(slides) {
             this.attributes.timer && this.autoPlay()
         },
         get current() {
-            return this.slides.find(slide => slide.id == this.state.order[this.state.currentSlide])
+            return this.state.currentSlide;
         },
         get previousSlide() {
-            return (this.state.currentSlide - 1) > -1 ? this.state.currentSlide - 1 : this.state.currentSlide
+            return (this.state.currentSlide - 1) > 0 ? this.state.currentSlide - 1 : this.slides.length
         },
         get nextSlide() {
-            return (this.state.currentSlide + 1) < this.slides.length ? this.state.currentSlide + 1 : this.state.currentSlide
+            return this.state.currentSlide < (this.slides.length - 1) ? this.state.currentSlide + 1 : 1
         },
         updateCurrent(nextSlide) {
             if (nextSlide == this.state.currentSlide) return
             if (this.state.moving) return
             this.state.moving = true
 
-            const next = this.slides.find(slide => slide.id == this.state.order[nextSlide])
+            const next = this.slides.find(slide_id => slide_id == this.state.order[nextSlide - 1])
 
             // Reorder the slides for a smoother transition
-            const newSlideOrder = this.slides.filter(slide => {
-                return ![this.current.id, this.state.order[nextSlide]].includes(slide.id)
+            const newSlideOrder = this.slides.filter(slide_id => {
+                return ![this.current, this.state.order[nextSlide - 1]].includes(slide_id)
             })
 
             const activeSlides = [this.current, next]
@@ -62,7 +58,7 @@ window.cultureSlideShow = function(slides) {
             }, this.attributes.duration)
 
         },
-        transitions(state, $dispatch) {
+        transitions(state) {
             const rightToLeft = this.state.nextSlideDirection === 'right-to-left'
             switch (state) {
                 case 'enter':
@@ -81,7 +77,7 @@ window.cultureSlideShow = function(slides) {
         },
         autoPlay() {
             this.loop = () => {
-                const next = (this.state.currentSlide === (this.slides.length - 1)) ? 0 : this.state.currentSlide + 1
+                const next = (this.state.currentSlide === this.slides.length) ? 1 : this.state.currentSlide + 1
                 this.updateCurrent(this.state.looping ? next : this.currentSlide)
                 this.autoplayTimer = setTimeout(() => {
                     requestAnimationFrame(this.loop)
