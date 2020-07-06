f2b0c9747cf021e46ebec8f49e0cd1beaac34f27
秦嘉鹏 <qinjiapeng@3lengjing.com>
2020-07-06 22:00:07 +0800

    删除console

--- app/javascript/controllers/main_controller.js
+++ app/javascript/controllers/main_controller.js
@@ -13,7 +13,6 @@ export default class extends Controller {
         this.element.style.height = `calc(100vh - ${this.element.offsetTop}px)`;
         
         if (this.hasFooterTarget && this.hasFooterLineTarget) {
-            console.log(this.footerTarget.offsetTop, this.footerLineTarget.offsetTop)
             if (this.footerTarget.offsetTop > this.footerLineTarget.offsetTop) {
                 if (this.footerTarget.offsetTop + this.footerTarget.offsetHeight < this.element.offsetHeight) {
                     this.footerTarget.style.position = 'absolute';
