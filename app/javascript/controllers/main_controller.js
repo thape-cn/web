5eb597173af2054f26ff3fc191560783ebbf1019
qinjiapeng <qinjia[eng@3lengjing.com>
2020-07-06 18:49:45 +0800

    解决IE等浏览器footer错位的问题

--- app/javascript/controllers/main_controller.js
+++ app/javascript/controllers/main_controller.js
@@ -1,6 +1,8 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
+    static targets = ['footer', 'footerLine'];
+
     connect() {
         this.setHeight();
         window.addEventListener('resize', this.setHeight);
@@ -8,6 +10,18 @@ export default class extends Controller {
 
     setHeight = () => {
         this.element.style.height = `calc(100vh - ${this.element.offsetTop}px)`;
+        
+        if (this.hasFooterTarget && this.hasFooterLineTarget) {
+            if (this.footerTarget.offsetTop > this.footerLineTarget.offsetTop) {
+                if (this.footerTarget.offsetTop + this.footerTarget.offsetHeight < this.element.offsetHeight) {
+                    this.footerTarget.style.position = 'absolute';
+                    this.footerTarget.style.bottom = '0px';
+                }
+            } else {
+                this.footerTarget.style.position = 'relative';
+                this.footerTarget.style.top = 'auto';
+            }
+        }
     }
 
     disconnect() {
