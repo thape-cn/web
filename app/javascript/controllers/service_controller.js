9dd4b4deabc1f63a5f9d4232f19fbc4d286bb1b4
Eric-Guo <eric.guocz@gmail.com>
2020-07-21 14:34:55 +0800

    Fix image height bug.

--- app/javascript/controllers/service_controller.js
+++ app/javascript/controllers/service_controller.js
@@ -4,17 +4,16 @@ export default class extends Controller {
   static targets = [ "bgImg", "introDiv" ]
 
   connect() {
-    this.bgImgOffsetHeight = this.bgImgTarget.offsetHeight;
   }
 
   resize() {
-    if (this.bgImgOffsetHeight >= this.element.offsetHeight) {
+    if (this.bgImgTarget.offsetHeight >= this.element.offsetHeight) {
       this.introDivTarget.classList.add("bg-thape-white-t80", 'sm:absolute');
       this.introDivTarget.classList.remove("bg-white", 'flex', 'content-center', 'flex-auto');
     } else {
       this.introDivTarget.classList.remove("bg-thape-white-t80");
       this.introDivTarget.classList.add("bg-white");
-      if (this.bgImgOffsetHeight + this.introDivTarget.offsetHeight <= this.element.offsetHeight) {
+      if (this.bgImgTarget.offsetHeight + this.introDivTarget.offsetHeight <= this.element.offsetHeight) {
         this.introDivTarget.classList.remove('sm:absolute');
         this.introDivTarget.classList.add('flex', 'content-center', 'flex-auto');
       } else {
