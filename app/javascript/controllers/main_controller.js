349a5d7d70a531726f7ad57c8553a6966fc48676
qinjiapeng <759848795@qq.com>
2020-07-29 16:49:00 +0800

    解决在手机端safari浏览器环境下作品页的footer无法正确定位的问题

--- app/javascript/controllers/main_controller.js
+++ app/javascript/controllers/main_controller.js
@@ -4,23 +4,27 @@ export default class extends Controller {
     static targets = ['footer', 'footerLine'];
 
     connect() {
-        this.setHeight();
         window.addEventListener('resize', this.setHeight);
         window.addEventListener('load', this.setHeight);
     }
 
     setHeight = () => {
         this.element.style.height = `calc(100vh - ${this.element.offsetTop}px)`;
-        
         if (this.hasFooterTarget && this.hasFooterLineTarget) {
-            if (this.footerTarget.offsetTop > this.footerLineTarget.offsetTop) {
-                if (this.footerTarget.offsetTop + this.footerTarget.offsetHeight < this.element.offsetHeight) {
-                    this.footerTarget.style.position = 'absolute';
-                    this.footerTarget.style.bottom = '0px';
-                }
+            if (this.footerTarget.offsetTop > this.footerLineTarget.offsetTop && this.footerLineTarget.offsetTop + this.footerTarget.offsetHeight < this.element.offsetHeight) {
+                this.footerTarget.style.position = 'absolute';
+                this.footerTarget.style.bottom = '0px';
             } else {
                 this.footerTarget.style.position = 'relative';
                 this.footerTarget.style.bottom = 'auto';
+                this.footerLineTarget.style.bottom = 'auto';
+                if (this.footerTarget.offsetTop + this.footerTarget.offsetHeight < this.element.scrollHeight) {
+                    const bottom = this.element.scrollHeight - this.footerTarget.offsetTop - this.footerTarget.offsetHeight + 240;
+                    this.footerTarget.style.bottom = `-${bottom}px`;
+                    this.footerLineTarget.style.bottom = `-${bottom}px`;
+                } else {
+                    this.footerTarget.style.bottom = 'auto';
+                }
             }
         }
     }
