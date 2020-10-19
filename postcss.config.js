cae347bad4510778a39807b8142b90c4552992ac
Eric-Guo <eric.guocz@gmail.com>
2020-10-19 09:23:07 +0800

    Refresh the PostCSS settings.

--- postcss.config.js
+++ postcss.config.js
@@ -23,7 +23,16 @@ if (process.env.RAILS_ENV === "production" || process.env.RAILS_ENV === "staging
         './app/javascript/**/*.js',
         './app/javascript/**/*.jsx',
       ],
-      defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || []
+      // This is the function used to extract class names from your templates
+      defaultExtractor: content => {
+        // Capture as liberally as possible, including things like `h-(screen-1.5)`
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+        // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+
+        return broadMatches.concat(innerMatches)
+      }
     })
   )
 }
