a068d2e1a2cbba5c26b5842f091e1f7c2d824baa
Eric-Guo <eric.guocz@gmail.com>
2020-11-03 11:09:16 +0800

    Only loading on development and test.

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-if !defined?(Spring) && [nil, "development", "test"].include?(ENV["RAILS_ENV"])
+if !defined?(Spring) && ["development", "test"].include?(ENV["RAILS_ENV"])
   load File.expand_path("../bin/spring", __dir__)
 end
 
