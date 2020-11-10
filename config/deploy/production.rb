6b1daa74565d3f64fc74fd3a176529baf9d54cfc
Eric-Guo <eric.guocz@gmail.com>
2020-11-10 10:06:46 +0800

    Deploy to main.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,6 @@
 set :nginx_use_ssl, true
 set :rails_env, 'production'
+set :branch, :main
 set :application, 'web'
 
 server 'www.thape.com', user: 'thape_web', roles: %w{app db web}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,5 @@
 set :nginx_use_ssl, true
 set :rails_env, 'staging'
+set :branch, :main
 
 server 'bandwagon', user: 'thape_web', roles: %w{app db web}
