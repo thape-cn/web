c0be9c13fe468b311764d034df859565af5fbe42
Eric-Guo <eric.guocz@gmail.com>
2020-12-27 17:35:16 +0800

    server_timing not working in ruby 3.0 yet.
    https://github.com/scoutapp/scout_apm_ruby/pull/366#issuecomment-751445164

--- Gemfile
+++ Gemfile
@@ -26,7 +26,6 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'browser'
-gem 'server_timing'
 
 gem 'canonical-rails'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 ﻿class ApplicationController < ActionController::Base
-  before_action do
-    ServerTiming::Auth.ok!
-  end
-
   before_action :set_locale
   before_action :set_ts
   before_action :prepare_seo_variable
