f7833151b23484089ebf48b3285b65d21b5a6e91
Eric-Guo <eric.guocz@gmail.com>
2021-01-14 15:45:18 +0800

    Pin the db/tianhua2020.sqlite3 files.

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :format_options, truncate: false
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, *%w[puma.rb config/database.yml config/master.key config/secrets.yml db/thape_prod.sqlite3 db/tianhua2019.sqlite3 config/credentials.yml.enc]
+append :linked_files, *%w[puma.rb config/database.yml config/master.key config/secrets.yml db/thape_prod.sqlite3 db/tianhua2019.sqlite3 db/tianhua2020.sqlite3 config/credentials.yml.enc]
 
 # Default value for linked_dirs is []
 append :linked_dirs, *%w[log tmp/pids tmp/cache tmp/sockets public/system public/packs public/uploads public/news_images node_modules]
