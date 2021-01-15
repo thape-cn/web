52d0d1f2d1536bf0e31a2e69fb86e9918e28590b
Eric-Guo <eric.guocz@gmail.com>
2021-01-15 13:00:55 +0800

    Fix migration.

--- db/migrate/20210115024130_add_p11_study_rate_to_tianhua2020.rb
+++
@@ -1,5 +0,0 @@
-class AddP11StudyRateToTianhua2020 < ActiveRecord::Migration[6.1]
-  def change
-    add_column :tianhua2020s, :p11_study_rate, :float
-  end
-end
--- db/tianhua2020_migrate/20210115024001_add_p10_teacher_level_to_tianhua2020.rb
+++ db/tianhua2020_migrate/20210115024001_add_p10_teacher_level_to_tianhua2020.rb
@@ -1,5 +1,6 @@
 class AddP10TeacherLevelToTianhua2020 < ActiveRecord::Migration[6.1]
   def change
     add_column :tianhua2020s, :p10_teacher_level, :string
+    add_column :tianhua2020s, :p11_study_rate, :float
   end
 end
