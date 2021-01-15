2c4838e23e5898198476689961fee0999a62a38c
Eric-Guo <eric.guocz@gmail.com>
2021-01-15 13:39:44 +0800

    Fix case when there is blank data.

--- app/controllers/tianhua2020s_controller.rb
+++ app/controllers/tianhua2020s_controller.rb
@@ -12,12 +12,24 @@ class Tianhua2020sController < ApplicationController
     @clerkcode = r.clerkcode
     p1_workbirthday = '<p style="margin-top: 0px; margin-bottom: 0px;"><span style="caret-color: rgb(228, 84, 73);"><b><font color="#df695a">1月22日</font></b></span><span style="color: rgb(41, 44, 54);">是我们的纪念日</span></p><p style="margin-top: 0px; margin-bottom: 0px;"><font>今年是我们相识的</font><b><font color="#df695a">' + (release_date - r.p1_firstday.to_date).ceil.to_s + '</font></b><font>天</font></p>'
 
-    p3_workrate = '<p style="margin-top: 0px; margin-bottom: 0px; color: rgb(252, 241, 216);">你的忙碌程度已超过</p><p style="margin-top: 0px; margin-bottom: 0px;"><font color="#f9bf3d"><b style="font-size: 50px;">' + (r.p3_workrate * 100).round(0).to_s + '%</b><span style="font-size: 50px;"> </span></font><font color="#fcf1d8">的小伙伴</font></p>'
-    p4_busymonth = '<font color="#f9bf3d" style="font-size: 50px;"><b>' + r.p4_busymonth.to_s + '月</b></font>是你最忙碌的一段时间，现在给自己五秒钟，回忆一下那段充实的时光吧'
-    p4_busy_week = '每周<b style="font-size: 50px;"><font color="#f9bf3d">'+ max_workday(r.p4_busyweek).to_s+'</font></b>通常是你战斗力爆表的时刻'
-    p4_workcom = '<span style="font-size: 50px;"><font color="#f9bf3d"><b>' + r.p4_busymonth.to_s + '月</b></font></span>是<br><font color="#f9bf3d"><b><span style="font-size: 50px;">' + r.p4_workcom.to_s + '</span><br></b></font>最忙碌的月份<br>'
-    p6_prjno = '今年你共参与了<span style="font-size: 50px;"><b><font color="#df695a">' + r.p6_prjno.to_s + '</font></b></span>个项目'
-    p6_totalarea = '<b style="font-size: 50px;"><font color="#df695a">' + r.p6_totalarea.to_s + '</font></b>平方米'
+    p3_workrate = if r.p3_workrate.present?
+      '<p style="margin-top: 0px; margin-bottom: 0px; color: rgb(252, 241, 216);">你的忙碌程度已超过</p><p style="margin-top: 0px; margin-bottom: 0px;"><font color="#f9bf3d"><b style="font-size: 50px;">' + (r.p3_workrate * 100).round(0).to_s + '%</b><span style="font-size: 50px;"> </span></font><font color="#fcf1d8">的小伙伴</font></p>'
+    end
+    p4_busymonth = if r.p4_busymonth.present?
+      '<font color="#f9bf3d" style="font-size: 50px;"><b>' + r.p4_busymonth.to_s + '月</b></font>是你最忙碌的一段时间，现在给自己五秒钟，回忆一下那段充实的时光吧'
+    end
+    p4_busy_week = if r.p4_busyweek.present?
+      '每周<b style="font-size: 50px;"><font color="#f9bf3d">'+ max_workday(r.p4_busyweek).to_s+'</font></b>通常是你战斗力爆表的时刻'
+    end
+    p4_workcom = if r.p4_busymonth.present?
+      '<span style="font-size: 50px;"><font color="#f9bf3d"><b>' + r.p4_busymonth.to_s + '月</b></font></span>是<br><font color="#f9bf3d"><b><span style="font-size: 50px;">' + r.p4_workcom.to_s + '</span><br></b></font>最忙碌的月份<br>'
+    end
+    p6_prjno = if r.p6_prjno.present?
+      '今年你共参与了<span style="font-size: 50px;"><b><font color="#df695a">' + r.p6_prjno.to_s + '</font></b></span>个项目'
+    end
+    p6_totalarea = if r.p6_totalarea.present?
+      '<b style="font-size: 50px;"><font color="#df695a">' + r.p6_totalarea.to_s + '</font></b>平方米'
+    end
 
     p6_city = if r.p6_city.present?
       r.p6_city.split(',').join(' &nbsp;')
@@ -40,13 +52,19 @@ class Tianhua2020sController < ApplicationController
       '<b style="font-size: 50px;"><font color="#f9bf3d">' + (r.p9_design_meeting_rate*100).round(0).to_s + '%</font></b>的小伙伴'
     end
 
-    p10_teacher_level = '作为天华的<b style="font-size: 50px;"><font color="#df695a">' + r.p10_teacher_level.to_s + '</font></b>讲师'
+    p10_teacher_level = if r.p10_teacher_level.present?
+      '作为天华的<b style="font-size: 50px;"><font color="#df695a">' + r.p10_teacher_level.to_s + '</font></b>讲师'
+    end
     p10_course_number = '<b><font color="#df695a" style="font-size: 50px;">' + r.p10_course_number.to_s + '</font></b>次课程'
 
     p11_finish_course = '在这一年，你共完成了<font color="#f9bf3d"><b>' + r.p11_finish_course.to_i.to_s + '</b></font>门在线'
-    p11_study_time = '课程的学习，总时长<font color="#f9bf3d"><b>' + r.p11_study_time.round(1).to_s + '</b></font>分钟'
+    p11_study_time = if r.p11_study_time.present?
+      '课程的学习，总时长<font color="#f9bf3d"><b>' + r.p11_study_time.round(1).to_s + '</b></font>分钟'
+    end
     p11_study_point = '可兑换<font color="#f9bf3d"><b>' + r.p11_study_point.to_s + '</b></font>积分'
-    p11_study_rate = '超过了<font color="#f9bf3d"><b>' + (r.p11_study_rate*100).round(0).to_s + '%</b></font>的小伙伴'
+    p11_study_rate = if r.p11_study_rate.present?
+      '超过了<font color="#f9bf3d"><b>' + (r.p11_study_rate*100).round(0).to_s + '%</b></font>的小伙伴'
+    end
     p11_study_award = r.p11_study_award.to_s + '<br><font color="#fdf3df" style="font-weight: normal;">学习证书</font>'
 
     p12_km_login_date = '共有<font color="#df695a"><b>' + r.p12_km_login_date.to_s + '</b></font>天，你登陆过KM'
