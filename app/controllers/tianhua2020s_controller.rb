ae21e3c2fdea09cca99d038bcf2da6048e72e039
Eric-Guo <eric.guocz@gmail.com>
2021-01-15 10:03:10 +0800

    Fix data filling bug.

--- app/controllers/tianhua2020s_controller.rb
+++ app/controllers/tianhua2020s_controller.rb
@@ -3,14 +3,12 @@ class Tianhua2020sController < ApplicationController
   layout "tianhua2020"
 
   def show
-    release_date = Date.new(2021, 1, 22)
+    release_date = DateTime.new(2021, 1, 22)
     r = Tianhua2020.find_by!(clerkcode: params[:id])
 
     @name = r.name
     @clerkcode = r.clerkcode
-
-    p1_workbirthday = '<p style="margin-top: 0px; margin-bottom: 0px;"><span style="caret-color: rgb(228, 84, 73);"><b><font color="#df695a">1月22日</font></b></span><span style="color: rgb(41, 44, 54);">是我们的纪念日</span></p>
-<p style="margin-top: 0px; margin-bottom: 0px;"><font>今年是我们相识的</font><b><font color="#df695a">' + (release_date - r.firstday).ceil.to_s + '</font></b><font>天</font></p>'
+    p1_workbirthday = '<p style="margin-top: 0px; margin-bottom: 0px;"><span style="caret-color: rgb(228, 84, 73);"><b><font color="#df695a">1月22日</font></b></span><span style="color: rgb(41, 44, 54);">是我们的纪念日</span></p><p style="margin-top: 0px; margin-bottom: 0px;"><font>今年是我们相识的</font><b><font color="#df695a">' + (release_date - r.p1_firstday.to_date).ceil.to_s + '</font></b><font>天</font></p>'
 
     p3_workrate = '<p style="margin-top: 0px; margin-bottom: 0px; color: rgb(252, 241, 216);">你的忙碌程度已超过</p><p style="margin-top: 0px; margin-bottom: 0px;"><font color="#f9bf3d"><b style="font-size: 50px;">' + (r.p3_workrate * 100).round(0).to_s + '%</b><span style="font-size: 50px;"> </span></font><font color="#fcf1d8">的小伙伴</font></p>'
     p4_busymonth = '<font color="#f9bf3d" style="font-size: 50px;"><b>' + r.p4_busymonth.to_s + '月</b></font>是你最忙碌的一段时间，现在给自己五秒钟，回忆一下那段充实的时光吧'
@@ -23,13 +21,22 @@ class Tianhua2020sController < ApplicationController
       r.p6_city.split(',').join(' &nbsp;')
     end
 
-    p8_design_prj = r.p8_design_prj.split('、').collect do |city|
-      "<div>#{city}</div>"
-    end.join
+    p8_design_prj = if r.p8_design_prj.present?
+      r.p8_design_prj.split('、').collect do |city|
+        "<div>#{city}</div>"
+      end.join
+    end
+    p9_design_meeting_times = if r.p9_design_meeting_times.present?
+      '你共参加了<b style="font-size: 50px;"><font color="#f9bf3d">' + r.p9_design_meeting_times.to_s + '</font></b>次设计例会'
+    end
 
-    p9_design_meeting_times = '你共参加了<b style="font-size: 50px;"><font color="#f9bf3d">' + r.p9_design_meeting_times.to_s + '</font></b>次设计例会'
-    p9_design_meeting_score = '以平均汇报成绩<font color="#f9bf3d" style="font-size: 50px;"><b>' + r.p9_design_meeting_score.round(1).to_s + '</b></font>分'
-    p9_design_meeting_rate = '<b style="font-size: 50px;"><font color="#f9bf3d">' + (r.p9_design_meeting_rate*100).round(0).to_s + '%</font></b>的小伙伴'
+    p9_design_meeting_score = if r.p9_design_meeting_score.present?
+      '以平均汇报成绩<font color="#f9bf3d" style="font-size: 50px;"><b>' + r.p9_design_meeting_score.round(1).to_s + '</b></font>分'
+    end
+
+    p9_design_meeting_rate = if r.p9_design_meeting_rate.present?
+      '<b style="font-size: 50px;"><font color="#f9bf3d">' + (r.p9_design_meeting_rate*100).round(0).to_s + '%</font></b>的小伙伴'
+    end
 
     p10_course_number = '<b><font color="#df695a" style="font-size: 50px;">' + r.p10_course_number.to_s + '</font></b>次课程'
 
