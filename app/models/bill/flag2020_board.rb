b90f3badcb375783300065084b3f6cc23918f79d
Eric-Guo <eric.guocz@gmail.com>
2021-01-22 15:24:13 +0800

    Skip if wecom_id empty.

--- app/models/bill/flag2020_board.rb
+++ app/models/bill/flag2020_board.rb
@@ -9,9 +9,11 @@ module Bill
       def sent_notify
         to_user = Bill::Tianhua2020.find_by clerkcode: to_clerkcode
         from_user = Bill::Tianhua2020.find_by clerkcode: from_clerkcode
-        Wechat.api.news_message_send(to_user.wecom_id, "#{from_user.name}立了一个Flag", '他@了你，请你监督',
-          "https://www.thape.com/tianhua2020s/#{to_user.clerkcode}/flag_board",
-          'https://thape-assets.oss-cn-shanghai.aliyuncs.com/packs/media/images/tianhua2020-notify-2961b89955b3f6cb617a840c06124bda.png')
+        if to_user.wecom_id.present?
+          Wechat.api.news_message_send(to_user.wecom_id, "#{from_user.name}立了一个Flag", '他@了你，请你监督',
+            "https://www.thape.com/tianhua2020s/#{to_user.clerkcode}/flag_board",
+            'https://thape-assets.oss-cn-shanghai.aliyuncs.com/packs/media/images/tianhua2020-notify-2961b89955b3f6cb617a840c06124bda.png')
+        end
       rescue Wechat::ResponseError
         true
       end
