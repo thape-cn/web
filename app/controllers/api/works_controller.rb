0a9b29f78a44a9fdef12cbaad1638b7bc0d7b44a
qinjiapeng <759848795@qq.com>
2021-02-04 16:05:01 +0800

    优化作品集列表API

--- app/controllers/api/works_controller.rb
+++ app/controllers/api/works_controller.rb
@@ -2,14 +2,14 @@
 
 class Api::WorksController < ApplicationController
   def index
-    works = Work.select(:id).order(position: :asc).where(published: true)
+    works = Work.select(:id).where(published: true)
     works_with_q = if params[:q].present?
       works.joins(:translations).where('work_translations.project_name LIKE ?', "%#{params[:q]}%")
     else
       works
     end
     @total = works_with_q.count
-    @works = Work.where(Work.arel_table[:id].in(works_with_q.pluck(:id))).includes(:translations)
+    @works = Work.where(Work.arel_table[:id].in(works_with_q.pluck(:id))).includes(:translations).order(position: :asc)
       .page(params[:page]).per(params[:per_page])
   end
 
