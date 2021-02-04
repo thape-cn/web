4d98ff5bd1bc891be92225ee5470d075bd88a64d
Eric-Guo <eric.guocz@gmail.com>
2021-02-04 15:41:53 +0800

    Fix missing english when calling http://localhost:3000/api/works.json?q=上海

--- app/controllers/api/works_controller.rb
+++ app/controllers/api/works_controller.rb
@@ -2,14 +2,15 @@
 
 class Api::WorksController < ApplicationController
   def index
-    works = Work.includes(:translations).order(position: :asc).where(published: true)
+    works = Work.select(:id).order(position: :asc).where(published: true)
     works_with_q = if params[:q].present?
       works.joins(:translations).where('work_translations.project_name LIKE ?', "%#{params[:q]}%")
     else
       works
     end
     @total = works_with_q.count
-    @works = works_with_q.page(params[:page]).per(params[:per_page])
+    @works = Work.where(Work.arel_table[:id].in(works_with_q.pluck(:id))).includes(:translations)
+      .page(params[:page]).per(params[:per_page])
   end
 
   def show
