c9874a539ef8faa58e27b461a3f885d6d5dca328
Eric-Guo <eric.guocz@gmail.com>
2021-03-31 15:18:17 +0800

    To staging server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 set :nginx_use_ssl, true
-set :rails_env, 'staging'
+set :rails_env, 'publication'
 set :branch, :main
 set :puma_service_unit_name, :puma_thape_web
 
