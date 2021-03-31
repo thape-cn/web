2232977d000b2d54c59b918a5aca13bcabf45804
Eric-Guo <eric.guocz@gmail.com>
2021-03-31 17:09:03 +0800

    Fix

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 set :nginx_use_ssl, true
-set :rails_env, 'publication'
-set :branch, :main
+set :rails_env, 'production'
+set :branch, :publication
 set :puma_service_unit_name, :puma_thape_web
 
 server 'bandwagon', user: 'thape_web', roles: %w{app db web}
