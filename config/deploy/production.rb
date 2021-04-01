a3e91b278effab964c69d5bfa4789f4d35e45de5
Eric-Guo <eric.guocz@gmail.com>
2021-04-01 15:20:27 +0800

    Deploy to main.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@
 
 set :nginx_use_ssl, true
 set :rails_env, 'production'
-set :branch, :publication
+set :branch, :main
 set :application, 'web'
 set :puma_service_unit_name, :puma_thape_web
 set :puma_systemctl_user, :system
