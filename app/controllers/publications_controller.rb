c1b7fa5a72383710baa4b099193c170a79170a8c
qinjiapeng <759848795@qq.com>
2021-04-06 17:38:55 +0800

    出版页样式与功能基本完成

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -10,9 +10,23 @@ class PublicationsController < ApplicationController
       { title: '天华2018作品集', subtitle: '居住建筑', url: '/book1-5.jpg', width: '85%', height: '85%' },
       { title: '天华2018作品集', subtitle: '公共建筑', url: '/book1-6.jpg', width: '85%', height: '85%' },
     ]
+
     @publications = [
-      { title: '上海城市更新', subtitle: '五种策略', url: '/book2-1.jpg', width: '100%', height: '100%' },
-      { title: 'ZHENGZHOU郑州', subtitle: '从火车之城到地铁大都会', url: '/book2-2.jpg', width: '100%', height: '77.8%' },
+      { title: '上海城市更新', subtitle: '五种策略', url: '/book2-1.jpg', width: '100%', height: '100%', category: 1 },
+      { title: 'ZHENGZHOU郑州', subtitle: '从火车之城到地铁大都会', url: '/book2-2.jpg', width: '100%', height: '77.8%', category: 1 },
+      { title: '上海城市更新', subtitle: '五种策略', url: '/book2-1.jpg', width: '100%', height: '100%', category: 2 },
+      { title: 'ZHENGZHOU郑州', subtitle: '从火车之城到地铁大都会', url: '/book2-2.jpg', width: '100%', height: '77.8%', category: 2 },
+      { title: '上海城市更新', subtitle: '五种策略', url: '/book2-1.jpg', width: '100%', height: '100%', category: 2 },
+      { title: '上海城市更新', subtitle: '五种策略', url: '/book2-1.jpg', width: '100%', height: '100%', category: 3 },
+      { title: 'ZHENGZHOU郑州', subtitle: '从火车之城到地铁大都会', url: '/book2-2.jpg', width: '100%', height: '77.8%', category: 3 },
+      { title: '上海城市更新', subtitle: '五种策略', url: '/book2-1.jpg', width: '100%', height: '100%', category: 3 },
+      { title: 'ZHENGZHOU郑州', subtitle: '从火车之城到地铁大都会', url: '/book2-2.jpg', width: '100%', height: '77.8%', category: 3 },
+    ]
+
+    @publication_categoty = [
+      { id: 1, title: '专著书籍' },
+      { id: 2, title: '规范标准' },
+      { id: 3, title: '论文专利' },
     ]
   end
 end
--- app/javascript/controllers/publications_controller.js
+++ app/javascript/controllers/publications_controller.js
@@ -1,6 +1,24 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  static targets = [ "marker", "categoryOrigin", "categoryFixed" ]
+
   connect() {
+    this.resize();
+  }
+
+  resize() {
+    const screenWidth = document.body.clientWidth;
+    if (screenWidth < 768) {
+      const originPz = this.categoryOriginTarget.getBoundingClientRect();
+      const where = originPz.y <= 50;
+      this.categoryFixedTarget.style.display = !where ? 'none' : '';
+    }
+  }
+
+  clickCate() {
+    const markerPz = this.markerTarget.getBoundingClientRect();
+    const curScrollTop = document.documentElement.scrollTop;
+    document.documentElement.scrollTo({ top: curScrollTop + markerPz.top - 50 });
   }
 }
