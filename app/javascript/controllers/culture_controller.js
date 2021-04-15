e6cc4b15ff0be70c68dabbf0ca443cac45c13c23
qinjiapeng <759848795@qq.com>
2021-04-15 11:28:16 +0800

    企业文化页面调整，图片放大，整体适配移动端视觉规范

--- app/javascript/controllers/culture_controller.js
+++ app/javascript/controllers/culture_controller.js
@@ -1,27 +1,33 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ['slider'];
+  static targets = ['sliders', 'dots'];
 
   connect() {
-    this.setSliderSize();
-    window.addEventListener('resize', this.setSliderSize);
+    this.setSlidersSize();
+    window.addEventListener('resize', this.setSlidersSize);
   }
 
-  setSliderSize = () => {
-    if (this.hasSliderTarget) {
-      const style = getComputedStyle(this.element);
-      const parentWidth = this.element.offsetWidth - parseInt(style['paddingLeft']) - parseInt(style['paddingRight']);
+  setSlidersSize = () => {
+    const footer = document.querySelector('footer');
+    const centerHeight = document.documentElement.clientHeight - this.element.offsetTop - footer.offsetHeight;
+    if (this.hasSlidersTarget) {
+      const parentStyle = getComputedStyle(this.element);
+      const parentWidth = this.element.offsetWidth - parseInt(parentStyle['paddingLeft']) - parseInt(parentStyle['paddingRight']);
       const imgWidth = parseInt(this.data.get('img-width'));
       const imgHeight = parseInt(this.data.get('img-height'));
-      const width = parentWidth > imgWidth ? imgWidth : parentWidth;
-      const height = imgHeight * width / imgWidth;
-      this.sliderTarget.style.width = `${width}px`;
-      this.sliderTarget.style.height = `${height}px`;
+      const slidersWidth = parentWidth > imgWidth ? imgWidth : parentWidth;
+      const slidersHeight = imgHeight * slidersWidth / imgWidth;
+      const dotsHeight = this.hasDotsTarget ? this.dotsTarget.offsetHeight : 0;
+      const centerMinHeight = slidersHeight + dotsHeight + parseInt(parentStyle['paddingTop']) + parseInt(parentStyle['paddingBottom']);
+      const parentHeight = centerHeight > centerMinHeight ? centerHeight : centerMinHeight;
+      this.element.style.height = `${parentHeight}px`;
+      this.slidersTarget.style.width = `${slidersWidth}px`;
+      this.slidersTarget.style.height = `${slidersHeight}px`;
     }
   }
 
   disconnect() {
-    window.removeEventListener('resize', this.setSliderSize);
+    window.removeEventListener('resize', this.setSlidersSize);
   }
 }
