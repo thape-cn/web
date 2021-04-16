75e3699ef5bd43f06087cddb61bde4b34178347b
qinjiapeng <759848795@qq.com>
2021-04-16 19:05:25 +0800

    修复出版物跨域下载功能

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -11,7 +11,7 @@ class PublicationsController < ApplicationController
         cover: item.cover_jpg || item.cover_webp,
         mobile_cover: item.mobile_cover_jpg || item.mobile_cover_webp,
         preview_url: item.pdf_file.present? ? "#{preview_conf_url}#{item.pdf_file}" : '',
-        download_url: item.pdf_file.present? ? item.pdf_file : '',
+        download_url: item.pdf_file.presence || '',
         width: "#{100 * item.pixel_width / 700}%",
         height: "#{100 * item.pixel_height / 900}%",
       }
@@ -26,7 +26,7 @@ class PublicationsController < ApplicationController
         cover: item.cover_jpg || item.cover_webp,
         mobile_cover: item.mobile_cover_jpg || item.mobile_cover_webp,
         preview_url: item.pdf_file.present? ? "#{preview_conf_url}#{item.pdf_file}" : '',
-        download_url: item.pdf_file.present? ? item.pdf_file : '',
+        download_url: item.pdf_file.presence || '',
         width: "#{100 * item.pixel_width / 700}%",
         height: "#{100 * item.pixel_height / 900}%",
         category: item.category_status,
--- app/javascript/controllers/publications_controller.js
+++ app/javascript/controllers/publications_controller.js
@@ -21,4 +21,24 @@ export default class extends Controller {
     const curScrollTop = document.documentElement.scrollTop;
     document.documentElement.scrollTo({ top: curScrollTop + markerPz.top - 50 });
   }
+
+  downloadFile(e) {
+    const url = e.target.dataset.url;
+    if (url) {
+      const ext = url.split('/').pop();
+      const x = new XMLHttpRequest();
+      x.open('GET', url);
+      x.responseType = 'blob';
+      x.onload = function(e) {
+        const blobUrl = window.URL.createObjectURL(x.response)
+        const a = document.createElement('a');
+        document.body.appendChild(a);
+        a.download = ext;
+        a.href = blobUrl;
+        a.click();
+        document.body.removeChild(a);
+      }
+      x.send();
+    }
+  }
 }
