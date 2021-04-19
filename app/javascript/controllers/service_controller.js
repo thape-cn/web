809fa3048770a336eeec80beae28d3e38cba8e0b
qinjiapeng <759848795@qq.com>
2021-04-19 15:22:00 +0800

    解决服务-可视化：头图太大

--- app/javascript/controllers/service_controller.js
+++ app/javascript/controllers/service_controller.js
@@ -4,21 +4,33 @@ export default class extends Controller {
   static targets = [ "bgImg", "introDiv" ]
 
   connect() {
+    this.resize();
   }
 
   resize() {
-    if (this.bgImgTarget.offsetHeight >= this.element.offsetHeight) {
-      this.introDivTarget.classList.add("bg-thape-white-t80", 'sm:absolute');
-      this.introDivTarget.classList.remove("bg-white", 'flex', 'content-center', 'flex-auto');
-    } else {
-      this.introDivTarget.classList.remove("bg-thape-white-t80");
-      this.introDivTarget.classList.add("bg-white");
-      if (this.bgImgTarget.offsetHeight + this.introDivTarget.offsetHeight <= this.element.offsetHeight) {
-        this.introDivTarget.classList.remove('sm:absolute');
-        this.introDivTarget.classList.add('flex', 'content-center', 'flex-auto');
+    if (this.hasBgImgTarget) {
+      const imgWidth = parseInt(this.data.get('imgWidth'));
+      const imgHeight = parseInt(this.data.get('imgHeight'));
+      const parentStyle = getComputedStyle(this.bgImgTarget);
+      const parentWidth = this.bgImgTarget.offsetWidth - parseInt(parentStyle['paddingLeft']) - parseInt(parentStyle['paddingRight']);
+      const parentHeight = imgHeight * parentWidth / imgWidth;
+      const serviceStyle = getComputedStyle(this.bgImgTarget);
+      const serviceHeight = this.element.offsetHeight - parseInt(serviceStyle['paddingLeft']) - parseInt(serviceStyle['paddingRight']);
+      const height = parentHeight > serviceHeight ? serviceHeight : parentHeight;
+      this.bgImgTarget.style.height = `${height}px`;
+      if (height >= this.element.offsetHeight) {
+        this.introDivTarget.classList.add("bg-thape-white-t80", 'lg:absolute');
+        this.introDivTarget.classList.remove("bg-white", 'flex', 'content-center', 'flex-auto');
       } else {
-        this.introDivTarget.classList.remove('flex', 'content-center', 'flex-auto');
-        this.introDivTarget.classList.add('sm:absolute');
+        this.introDivTarget.classList.remove("bg-thape-white-t80");
+        this.introDivTarget.classList.add("bg-white");
+        if (height + this.introDivTarget.offsetHeight <= this.element.offsetHeight) {
+          this.introDivTarget.classList.remove('lg:absolute');
+          this.introDivTarget.classList.add('flex', 'content-center', 'flex-auto');
+        } else {
+          this.introDivTarget.classList.remove('flex', 'content-center', 'flex-auto');
+          this.introDivTarget.classList.add('lg:absolute');
+        }
       }
     }
   }
