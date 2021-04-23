0c2a8eaabe6b8985520947b9621ed91dbf719493
Eric-Guo <eric.guocz@gmail.com>
2021-04-23 11:27:29 +0800

    Now staging run main.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 set :nginx_use_ssl, true
 set :rails_env, 'staging'
-set :branch, :publication
+set :branch, :main
 set :puma_service_unit_name, :puma_thape_web
 
 server 'bandwagon', user: 'thape_web', roles: %w{app db web}
