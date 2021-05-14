f781a3a4a4c634e71dd192f9953fb12f4a5cbd3a
Eric-Guo <eric.guocz@gmail.com>
2021-05-14 15:07:11 +0800

    To generate the works.

--- app/models/work.rb
+++ app/models/work.rb
@@ -17,4 +17,23 @@ class Work < ApplicationRecord
 
   translates :project_name, :client, :services, :team, :cooperation, :awards,
     :design_completion_lines, :construction_completion_lines, :architecture_area_lines
+
+  def write_tianwen_xml(tmpl)
+    tmpl.at_css('TRS_PRIMARY').content = id
+    tmpl.at_css('TRS_TITLE').content = project_name
+    tmpl.at_css('TRS_CONTENT').content = client
+    tmpl.at_css('TRS_CREATETIME').content = created_at.to_s(:db)
+    tmpl.at_css('TRS_ORG').content = team
+    tmpl.at_css('TRS_CATEGORY2').content = project_types.collect(&:cn_name)&.join(',')
+    tmpl.at_css('TRS_BACKLINK').content = Rails.application.routes.url_helpers.work_url(id: id)
+    tmpl.at_css('TRS_ATTR1').content = snapshot_jpg.url(thumb: '?x-oss-process=image/resize,w_327')
+    tmpl.at_css('TRS_CRESERVED3').content = "#{coordinate_lat},#{coordinate_lng}"
+    tmpl.at_css('TRS_CRESERVED4').content = awards
+    tmpl.at_css('TRS_OPP').content = 1 # 1 for create, 2 for modify
+
+    tgt_dir = Rails.root.join('public', 'tianwen_sync', "works_#{id}.xml")
+    File.open(tgt_dir, 'wb') do |f|
+      f.write(tmpl.to_xml)
+    end
+  end
 end
--- lib/tasks/tianwen_sync.rake
+++ lib/tasks/tianwen_sync.rake
@@ -2,7 +2,7 @@
 
 namespace :tianwen_sync do
   desc 'Sync all news'
-  task all: %i[sync_news]
+  task all: %i[sync_news sync_works]
 
   desc 'Sync news'
   task sync_news: :environment do
@@ -11,6 +11,16 @@ namespace :tianwen_sync do
 
     Info.all.find_each do |info|
       info.write_tianwen_xml(tmpl)
+    end
+  end
+
+  desc 'Sync works'
+  task sync_works: :environment do
+    e1_tmpl_path = Rails.root.join('public', 'tianwen_tile', 'E1.xml')
+    tmpl = File.open(e1_tmpl_path) { |f| Nokogiri::XML(f) }
+
+    Work.all.find_each do |work|
+      work.write_tianwen_xml(tmpl)
     end
   end
 end
