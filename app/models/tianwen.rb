29d8b8a231a5ddc4ff99cec2d0e60885ada9099c
qinjiapeng <759848795@qq.com>
2021-08-25 14:46:58 +0800

    增加天问xml文件读写权限

--- app/models/tianwen.rb
+++ app/models/tianwen.rb
@@ -2,10 +2,11 @@ class Tianwen
   def self.xml_to_dir(content, file_name)
     tgt_dir = Rails.configuration.tianwen_dir.join("#{file_name}.xml")
     File.open(tgt_dir, 'wb') do |f|
-      f.write(content)
+      f.chmode(0664)
       if ENV['RAILS_ENV'] == 'production'
         f.chown(Rails.configuration.tianwen_uid, Rails.configuration.tianwen_gid)
       end
+      f.write(content)
     end
   end
 end
