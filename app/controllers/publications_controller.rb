39fba770255feb72f59254abae27960dbe5b99e4
qinjiapeng <759848795@qq.com>
2022-06-09 11:40:17 +0800

    新增出版物-天华内刊

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -38,5 +38,20 @@ class PublicationsController < ApplicationController
       { name: 'standard_specification', title: t('publication.standard_specification') },
       { name: 'paper_patent', title: t('publication.paper_patent') },
     ]
+
+    insights = Insight.order(position: :asc).all
+    @insights = insights.map do |item|
+      {
+        title: item.title,
+        subtitle: item.sub_title,
+        cover: item.cover_jpg.url || item.cover_webp.url,
+        mobile_cover: item.mobile_cover_jpg.url || item.mobile_cover_webp.url,
+        page_cover: item.page_cover.url,
+        preview_url: helpers.pdf_view_url(item.pdf_file.url),
+        download_url: item.pdf_file.url,
+        width: "#{100 * item.pixel_width / 700}%",
+        height: "#{100 * item.pixel_height / 900}%",
+      }
+    end
   end
 end
