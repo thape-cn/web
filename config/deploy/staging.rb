7d8c8f170b9554d44e40d92784825f0c8304555a
Eric Guo <eric.guocz@gmail.com>
2022-11-04 15:08:22 +0800

    Using system user.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,5 +4,6 @@ set :nginx_use_ssl, true
 set :rails_env, 'staging'
 set :branch, :main
 set :puma_service_unit_name, :puma_thape_web
+set :puma_systemctl_user, :system
 
 server 'bandwagon', user: 'thape_web', roles: %w{app db web}
