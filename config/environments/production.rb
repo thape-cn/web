ac45ea291df2d875648411763c6882abc0461ea1
Eric Guo <eric.guocz@gmail.com>
2024-04-22 11:07:33 +0800

    Fix warning.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # We using sqlite3 in production
+  config.active_record.sqlite3_production_warning = false
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
