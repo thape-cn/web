c6f11751a6875fbc63dc4a5c687ebfe13c8eb6d8
Eric Guo <eric.guocz@gmail.com>
2024-07-11 09:53:50 +0800

    Deploy to 3.3.4

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@ append :linked_dirs, *%w[log tmp/pids tmp/cache tmp/sockets public/system public
 # set :ssh_options, verify_host_key: :secure
 
 set :rbenv_type, :user
-set :rbenv_ruby, "3.3.3"
+set :rbenv_ruby, "3.3.4"
 
 set :puma_init_active_record, true
 set :puma_phased_restart, true
