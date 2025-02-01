9e6b02de31ad22d29051460a490edb6e2b8653a0
Eric Guo <eric.guocz@gmail.com>
2025-02-01 20:42:19 +0800

    Some ignore.

--- config/initializers/rorvswild.rb
+++ config/initializers/rorvswild.rb
@@ -1,3 +1,9 @@
 if Rails.env.production?
-  RorVsWild.start(api_key: Rails.application.credentials.rorvswild_api_key)
+  RorVsWild.start(
+    api_key: Rails.application.credentials.rorvswild_api_key,
+    ignore_requests: ["SitesController#privacy"],
+    ignore_jobs: [],
+    ignore_exceptions: [],
+    ignore_plugins: ["Elasticsearch","Mongo","Resque","DelayedJob","Sidekiq"]
+    )
 end
