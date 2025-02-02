4e456f97ffe8fd4745ff90873f968b0eaefc495e
Eric Guo <eric.guocz@gmail.com>
2025-02-02 09:25:36 +0800

    Safe to ignore.

--- config/initializers/rorvswild.rb
+++ config/initializers/rorvswild.rb
@@ -3,7 +3,7 @@ if Rails.env.production?
     api_key: Rails.application.credentials.rorvswild_api_key,
     ignore_requests: ["SitesController#privacy"],
     ignore_jobs: [],
-    ignore_exceptions: [],
+    ignore_exceptions: ["ActionController::UnknownFormat"],
     ignore_plugins: ["Elasticsearch","Mongo","Resque","DelayedJob","Sidekiq"]
     )
 end
