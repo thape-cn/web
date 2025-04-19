b86981c47693b69400f64a6bea1c9acc8b8d296a
Eric-Guo <eric.guocz@gmail.com>
2025-04-20 07:02:03 +0800

    Reapply "Remove rorvswild as it will exceed the usage of API."

---
+++ config/initializers/rorvswild.rb
@@ -0,0 +1,9 @@
+if Rails.env.production?
+  RorVsWild.start(
+    api_key: Rails.application.credentials.rorvswild_api_key,
+    ignore_requests: [],
+    ignore_jobs: [],
+    ignore_exceptions: ["ActionController::UnknownFormat"],
+    ignore_plugins: ["Elasticsearch","Mongo","Resque","DelayedJob","Sidekiq"]
+    )
+end
