47b140514ca55463240f00a683e9be30e4604352
Eric-Guo <eric.guocz@gmail.com>
2026-07-16 16:34:02 +0800

    Do notify via WeCom

--- app/controllers/project_messages_controller.rb
+++ app/controllers/project_messages_controller.rb
@@ -3,6 +3,7 @@
 class ProjectMessagesController < ApplicationController
   def create
     @project_message = ProjectMessage.create(project_message_params)
+    notify_cybros if @project_message.persisted?
 
     respond_to do |format|
       format.js
@@ -11,6 +12,29 @@ class ProjectMessagesController < ApplicationController
   end
 
   private
+
+  def notify_cybros
+    response = Faraday.post(
+      Rails.application.credentials.cybros_notification_url,
+      {
+        secret: Rails.application.credentials.project_message_secret,
+        project_message: @project_message.as_json(only: %i[
+          name phone company project_type service_type project_area message
+          spam_score created_at updated_at
+        ])
+      }.to_json,
+      "Content-Type" => "application/json"
+    ) do |request|
+      request.options.open_timeout = 2
+      request.options.timeout = 5
+    end
+
+    return if response.success?
+
+    Rails.logger.error("Cybros project message notification failed with HTTP #{response.status}")
+  rescue Faraday::Error => e
+    Rails.logger.error("Cybros project message notification failed: #{e.message}")
+  end
 
   def project_message_params
     params.require(:project_message)
--- test/controllers/project_messages_controller_test.rb
+++ test/controllers/project_messages_controller_test.rb
@@ -4,23 +4,38 @@ require "test_helper"
 
 class ProjectMessagesControllerTest < ActionDispatch::IntegrationTest
   test "creates project message with selected text values" do
-    assert_difference("ProjectMessage.count") do
-      post project_messages_path, params: {
-        project_message: {
-          name: "张三",
-          phone: "13800138000",
-          company: "天华测试",
-          project_type: "住宅、商业",
-          service_type: "方案、施工图",
-          project_area: "10000",
-          message: "项目合作信息"
+    notification = nil
+    response = Struct.new(:success?, :status).new(true, 200)
+
+    faraday_post = lambda do |*arguments|
+      notification = arguments
+      response
+    end
+
+    Faraday.stub(:post, faraday_post) do
+      assert_difference("ProjectMessage.count") do
+        post project_messages_path, params: {
+          project_message: {
+            name: "张三",
+            phone: "13800138000",
+            company: "天华测试",
+            project_type: "住宅、商业",
+            service_type: "方案、施工图",
+            project_area: "10000",
+            message: "项目合作信息"
+          }
         }
-      }
+      end
     end
 
     project_message = ProjectMessage.order(:created_at).last
     assert_equal "住宅、商业", project_message.project_type
     assert_equal "方案、施工图", project_message.service_type
+    assert_equal Rails.application.credentials.cybros_notification_url, notification.first
+    payload = JSON.parse(notification.second)
+    assert_equal Rails.application.credentials.project_message_secret, payload["secret"]
+    assert_equal "张三", payload.dig("project_message", "name")
+    assert_equal project_message.created_at.iso8601(3), payload.dig("project_message", "created_at")
     assert_redirected_to biz_map_path
   end
 end
