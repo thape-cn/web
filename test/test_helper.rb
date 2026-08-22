548557c9035ddd7e47f294011e5ca43887723343
Eric-Guo <eric.guocz@gmail.com>
2026-08-22 10:10:39 +0800

    Shakapacker 10.3.1 migration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/mock"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
